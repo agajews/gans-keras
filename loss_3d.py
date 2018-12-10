2204de4d3343b4b142a2348311612b016051a9a1
Alex Gajewski <agajews@gmail.com>
2018-12-10 13:58:46 -0500

    Added 3d

---
+++ loss_3d.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+loss = np.load('DCGAN_loss_n_samples_500_large.npy')
+xs = np.linspace(-2, 2, loss.shape[0])
+ys = np.linspace(-2, 2, loss.shape[1])
+xx, yy = np.meshgrid(xs, ys)
+
+ax = plt.axes(projection='3d')
+# ax.contour3D(xx, yy, loss, 50, cmap='binary')
+ax.plot_surface(xx, yy, loss, rstride=1, cstride=1,
+                cmap='viridis', edgecolor='none')
+
+plt.show()
